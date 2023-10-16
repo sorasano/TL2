0caa26a44be9897b705079c3a31310871b3b7d9f
SoraSano <bigsky.200315@gmail.com>
2023-10-16 14:53:02 +0900

    02_04

---
+++ TextureConverter.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+class TextureConverter
+{
+public:
+
+	void ConvertTextureWICToDDS(const std::string& filePath);
+
+private:
+
+	void LoadWICTextureFromFile(const std::string &filePath);
+
+	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+};
+
--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <cstdio>
 #include <cstdlib>
+#include <DirectXTex.h>
+
+#include "TextureConverter.h"
+
+enum Argument {
+	kApplicationPath,
+	kFilePath,
+	NumArgument
+};
 
 int main(int argc,char* argv[]) {
 
-	for (int i = 0; i < argc; i++) {
-		printf(argv[i]);
-		//‰üs
-		printf("\n");
-	}
+	assert(argc >= NumArgument);
+
+	HRESULT hr = CoInitializeEx(nullptr,COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	TextureConverter converter;
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	CoUninitialize();
 
 	system("pause");
 	return 0;
